34459c24420beb39bbab07187f615ca870812487
hitcrakcer317 <dainaso97@gmail.com>
2017-06-08 10:20:15 +0900

    一通りの機能を実装

--- SaveText/ViewController.swift
+++ SaveText/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        textView.textContainer.lineFragmentPadding = 0
+        textView.textContainerInset = UIEdgeInsetsMake(10, 10, 10, 10)
+        
+        
+        let ud = UserDefaults.standard
+        //let textValue = ud.string(forKey: "text")
+        
+        //UserDefaultのインスタンス.取り出す値の型(forKey: "キーの文字列")　UserDefalutから値を取り出す
+        //保存された値が存在するかどうかチェック
+        if let textValue = ud.string(forKey: "text") {
+            textView.text = textValue
+        } else {
+            textView.text = "テキストを入力"
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +40,18 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func saveText(_ sender: Any) {
+        //テキストを保存
+        let ud = UserDefaults.standard
+        ud.set(textView.text, forKey: "text")
+        ud.synchronize()
+    }
+    
+    @IBAction func deleteText(_ sender: Any) {
+        //テキストを削除
+        let ud = UserDefaults.standard
+        ud.removeObject(forKey: "text")
+        textView.text = ""
+    }
 }
 
